60be48eeb56781c4b2e211644f1eb11706958f0b
Mirwali Abdali <mirwaliabdali380@gmail.com>
2024-08-21 21:05:12 +0430

    Add migration route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ReservationsController::class, 'index'])->name('index');
 Route::post('/store', [ReservationsController::class, 'store'])->name('store');
+Route::get('/migrate', function () {
+  \Artisan::call('migrate');
+  return 'Migration run successfully!';
+});
